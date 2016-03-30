2f0b755f40d66afbc47af5485e78a1df6a6573a6
WY08271 <wy08271@outlook.com>
2016-03-30 12:54:07 +0800

    add , ,

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,22 +1,46 @@
-$(document).ready(function () {
-  $('#screenNumber').val('0');
+$(document).ready(function() {
+    var service = new Service();
 
-  $('.number-button').on('click', function () {
-    var screenNumber = $('#screenNumber').val();
-    var inputNumber = this.id;
+    setScreen('0');
 
-    var result = linkNumber(screenNumber, inputNumber);
-    $('#screenNumber').val(result);
+    $('#AC').on('click', function() {
+        service.one = undefined;
+        service.two = undefined;
+        service.symbol = undefined;
+        service.single = undefined;
 
-  })
+        setScreen('0');
+    });
+
+    $('.opposite-button').on('click', function() {
+        service.single = $('#screenNumber').val();
+        service.opposite(service, setScreen);
+    });
+
+    $('.percent-button').on('click', function() {
+        service.single = $('#screenNumber').val();
+        service.percent(service, setScreen);
+    });
+
+    $('.number-button').on('click', function() {
+        var screenNumber = $('#screenNumber').val();
+        var inputNumber = this.id;
+
+        var result = linkNumber(screenNumber, inputNumber);
+        $('#screenNumber').val(result);
+    });
 });
 
+function setScreen(number) {
+    $('#screenNumber').val(number);
+}
+
 function linkNumber(screenNumber, inputNumber) {
-  if (screenNumber === '0') {
-    return inputNumber;
-  } else if (inputNumber === 'point') {
-    return screenNumber + '.';
-  } else{
-    return screenNumber + inputNumber;
-  }
+    if (screenNumber === '0') {
+        return inputNumber;
+    } else if (inputNumber === 'point') {
+        return screenNumber + '.';
+    } else {
+        return screenNumber + inputNumber;
+    }
 }
